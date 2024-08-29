f0907632c9bcb58d7bb2c65e36b5606c0f827ffb
Harold Julio Flores Suazo <harflores.cl@gmail.com>
2024-08-29 10:28:55 -0400

    first commit

---
+++ src/main/java/com/environment/SpringEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEnvironmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/environment/SpringEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
